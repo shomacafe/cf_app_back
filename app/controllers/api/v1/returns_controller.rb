58de678ce85ee9adcb27c29ff161b682934f8260
shomacafe <shoma21912@gmail.com>
2023-08-10 01:09:24 +0900

    add ログイン機能を実装するためdevise_token_authによる認証などの追加

---
+++ app/controllers/api/v1/returns_controller.rb
@@ -0,0 +1,56 @@
+class Api::V1::ReturnsController < ApplicationController
+  before_action :find_return, only: [:update, :destroy]
+
+  def index
+    @returns = Return.where(project_id: params[:project_id])
+    if @returns.any?
+      render json: @returns
+    else
+      render json: { error: "Not Found" }, status: :not_found
+    end
+  end
+
+  def show
+    @return = Return.where(project_id: params[:project_id], id: params[:id]).first
+    if @return
+      render json: @return
+    else
+      render json: { error: "Not Found" }, status: :not_found
+    end
+  end
+
+  def create
+    @return = Return.new(return_params)
+
+    if @return.save
+      render json: @return, status: :created
+    else
+      render json: @return.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @return.update(return_params)
+      render json: @return, status: :ok
+    else
+      render json: @return.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @return.destroy
+    head :no_content
+  end
+
+  private
+
+  def find_return
+    @return = Return.find(params[:id])
+  end
+
+  def return_params
+    params.require(:return).map do |param|
+      param.permit(:id, :project_id, :name, :price, :return_image, :description, :stock_count, :_destroy)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :projects, only: [:index, :create, :show, :update, :destroy]
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+      namespace :auth do
+        resources :sessions, only: [:index]
+      end
+      resources :projects, only: [:index, :show, :create, :update, :destroy] do
+        resources :returns, only: [:index, :show, :create, :update]
+      end
+      resources :users, only: [:index, :show, :update]
     end
   end
 end
