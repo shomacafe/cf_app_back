3c24426e70e37cfd3ccebd37cea71e0480c3a917
shomacafe <shoma21912@gmail.com>
2023-09-08 21:57:15 +0900

    update Projectテーブルにおけるproject_imagesカラムをtextからjsonに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_25_091855) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_08_125532) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
